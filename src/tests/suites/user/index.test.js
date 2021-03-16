8f42fcd644042f8519ec6009f872fceedefe26c1
Dhruvil Shah <dsdruvil8@gmail.com>
2021-03-16 20:03:28 +0530

    updated(src/tests/suites/user/): adding user tests, updated(package.json): adding tests to run user specific tests

---
+++ src/tests/suites/user/index.test.js
@@ -0,0 +1,63 @@
+describe("User Tests", () => {
+	this.tags = ["user"];
+
+	beforeEach((browser, done) => {
+		browser.url("https://bstackdemo.com").assert.title("StackDemo");
+		done();
+	});
+
+	afterEach((browser, done) => {
+		browser.execute("sessionStorage.clear()");
+		done();
+	});
+
+	it("Item Images not available for 'image_not_loading_user'", (browser) => {
+		browser
+			.click("#signin")
+			.clearValue("#username input")
+			.setValue("#username input", "image_not_loading_user\n")
+			.clearValue("#password input")
+			.setValue("#password input", "testingisfun99\n")
+			.click("#login-btn")
+			.pause(1000)
+			.assert.containsText(".username", "image_not_loading_user");
+	});
+
+	it("Number of orders != 0", (browser) => {
+		browser
+			.refresh()
+			.click("#signin")
+			.clearValue("#username input")
+			.setValue("#username input", "existing_orders_user\n")
+			.clearValue("#password input")
+			.setValue("#password input", "testingisfun99\n")
+			.click("#login-btn")
+			.pause(1000)
+			.assert.containsText(".username", "existing_orders_user")
+			.click("#orders")
+			.expect.elements(".order")
+			.count.to.not.equal(0);
+	});
+
+	it("Number of favouriters != 0", (browser) => {
+		browser
+			.refresh()
+			.click("#signin")
+			.clearValue("#username input")
+			.setValue("#username input", "existing_orders_user\n")
+			.clearValue("#password input")
+			.setValue("#password input", "testingisfun99\n")
+			.click("#login-btn")
+			.pause(1000)
+			.assert.containsText(".username", "existing_orders_user")
+			.click("xpath", "//p[text() = 'iPhone 12']/../div/button")
+			.click("#favourites")
+			.pause(1000)
+			.assert.containsText("p.shelf-item__title", "iPhone 12");
+	});
+
+	after((browser, done) => {
+		browser.end();
+		done();
+	});
+});
