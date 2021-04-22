a985fe2cd7778be01242c7b8918b903d0bdc2661
Dhruvil Shah <dsdruvil8@gmail.com>
2021-04-22 20:51:41 +0530

    updated(src/conf/runners/browserstack_local.js): reading local args from capabilities file, updated(src/conf/caps/browserstack.json): adding local_args capability

--- resources/conf/runners/browserstack_local.js
+++ resources/conf/runners/browserstack_local.js
@@ -2,9 +2,13 @@
 
 const Nightwatch = require("nightwatch");
 const browserstack = require("browserstack-local");
+const Capabilities = require("../caps/browserstack.json");
+
 let bs_local;
 
 try {
+	const accessKey =
+		process.env.BROWSERSTACK_ACCESS_KEY || Capabilities.access_key;
 	const pathToNightwatchExecutable = "./node_modules/nightwatch/bin/nightwatch";
 	require.main.filename = pathToNightwatchExecutable;
 	process.mainModule.filename = pathToNightwatchExecutable;
@@ -12,7 +16,7 @@ try {
 	console.log("Connecting local");
 	Nightwatch.bs_local = bs_local = new browserstack.Local();
 	bs_local.start(
-		{ key: process.env.BROWSERSTACK_ACCESS_KEY },
+		{ key: accessKey, ...Capabilities.local_args },
 		function (error) {
 			if (error) throw error;
 
